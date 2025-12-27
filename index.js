3a2cebe7bd74d0384455f05137a8b119fb1294e5
tonyhusband <tonyhusband@Adritas-Air.lan>
2025-12-26 21:13:16 -0800

    added json and icon

--- index.js
+++ index.js
@@ -1,1 +1,27 @@
+let myLeads = []
+const inputEl = document.getElementById("input-el")
+const inputBtn = document.getElementById("input-btn")
+const ulEl=document.getElementById("un-list")
 
+
+inputBtn.addEventListener("click", function() {
+    myLeads.push(inputEl.value)
+    console.log("Button clicked!")
+  console.log(myLeads)
+renderLeads()
+inputEl.value= ""
+})
+
+
+function renderLeads (){
+  let listItems=""
+
+for (let i = 0; i < myLeads.length; i++) {
+listItems+= `<li>
+    <a target='_blank' href='${myLeads[i]}'>
+        ${myLeads[i]}
+    </a>
+</li>`
+}
+ulEl.innerHTML=listItems
+}
